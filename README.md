64f92820b94cba6810fe75bba6ecee2b350f128e
Luke Segars <luke@theironyard.com>
2015-11-14 22:43:33 -0600

    First layout proposal
    Also includes gulpfile for easy iteration and development. Need to add
    in media queries to handle smaller screen sizes but works well on most
    desktop screens as-is. No JS included yet.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # inoverourheadsshow.com
-Generators for inoverourheadsshow.com content.
+Run `gulp` to generate all public-facing content. Content will all be added to the `public/` folder, which can be served directly.
+
+Audio content is not present in this repo but can be downloaded using a soon-to-be-available script.
+
+## TODO items
+- **[DONE]** Core content layout
+- **[DONE]** SCSS styling
+- **[DONE]** Gulpfile
+- **[DONE]** RSS generator and validator
+- Finalize logo
+- Finalize design
+- Write content
+- Frontend JS / show notes loading
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat'); // join a group of files (gulp.dest) into a single file
+var sass = require('gulp-sass');
+
+var paths = { in : {
+        jade: './views/*.jade',
+        scss: './styles/*.scss',
+        js: './appjs/*.js',
+    },
+    out: {
+        html: './public',
+        css: './public/css',
+        js: './public/js',
+    }
+};
+
+gulp.task('default', ['templates', 'styles', 'js', 'test']);
+
+gulp.task('templates', function () {
+    return gulp.src(paths.in.jade)
+        .pipe(jade())
+        .pipe(gulp.dest(paths.out.html));
+});
+
+gulp.task('styles', function () {
+    return gulp.src(paths.in.scss)
+        .pipe(sass({
+            includePaths: require('node-normalize-scss').includePaths
+        }).on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest(paths.out.css));
+});
+
+gulp.task('js', function () {
+
+});
+
+gulp.task('test', function () {
+
+});
+
+gulp.task('watch', ['templates', 'styles', 'js'], function () {
+    gulp.watch(paths.in.jade, ['templates']);
+    gulp.watch(paths.in.scss, ['styles']);
+    //    gulp.watch(paths.in.js, ['js']);
+});
